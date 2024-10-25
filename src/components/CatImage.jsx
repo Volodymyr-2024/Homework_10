7c6362cdfdad30f024d1ab57becf17f870cfd3d9
Volodymyr-2024 <itspec.ua@gmail.com>
2024-10-25 22:16:37 +0200

    Added loading 25.10.2024

--- src/components/CatImage.jsx
+++ src/components/CatImage.jsx
@@ -5,15 +5,19 @@ import styles from "./CatImage.module.css";
 
 export default function CatImage() {
   const [catImage, setCatImage] = useState(null);
+  const [loading, setLoading] = useState(true);
   const CatAPI = "https://api.thecatapi.com/v1/images/search";
 
   async function fetchCatAPI() {
+    setLoading(true);
     try {
       const response = await axios.get(CatAPI);
       const data = response.data;
       setCatImage(data[0].url);
     } catch (error) {
       console.error("Ошибка при получении изображения", error);
+    } finally {
+      setLoading(false);
     }
   }
   useEffect(() => {
@@ -23,10 +27,10 @@ export default function CatImage() {
     <div className={styles.container}>
       <h1>Random Cat Image</h1>
       <div className={styles.cat_img}>
-        {catImage ? (
-          <img src={catImage} alt="Random Cat"></img>
+        {loading ? (
+          <p>Downloads ...</p>
         ) : (
-          <p>Downloads...</p>
+          <img src={catImage} alt="Random Cat"></img>
         )}
       </div>
       <button onClick={fetchCatAPI}>Load New Image</button>
